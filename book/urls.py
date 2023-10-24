78a7982f449538735c91d8de0f236f8b0812b4ad
sabrinaatha <sabrinaathashania@gmail.com>
2023-10-25 02:06:57 +0700

    starting login page and signup page

--- book/urls.py
+++ book/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from book.views import get_books_by_json, get_books_by_xml
+from book.views import get_books_by_json, get_books_by_xml, register, login_user, logout_user, show_main
 from . import views
+
 
 app_name = 'book'
 
 urlpatterns = [
-    path('json', get_books_by_json, name='get_book_by_jsons'),
-    path('xml', get_books_by_xml, name='get_by_xml'),
+    path('', show_main, name='show_main'),
+    path('json/', get_books_by_json, name='get_book_by_jsons'),
+    path('xml/', get_books_by_xml, name='get_by_xml'),
+    path('register/', register, name='register'), 
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
