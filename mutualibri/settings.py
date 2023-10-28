48132242afd5bb5418ca4a35068d1683bb800633
Samuelwidjaja <james.samuel.widjaja@gmail.com>
2023-10-28 21:33:36 +0700

    modul search

--- mutualibri/settings.py
+++ mutualibri/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'book',
-    'lend',
 ]
 
 MIDDLEWARE = [
@@ -56,7 +55,7 @@ ROOT_URLCONF = 'mutualibri.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
