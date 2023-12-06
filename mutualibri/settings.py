a12f1e2757fa471dd43d79728e1bfd0d63646a2d
sabrinaatha <sabrinaathashania@gmail.com>
2023-12-06 15:02:46 +0700

    add csrf security

--- mutualibri/settings.py
+++ mutualibri/settings.py
@@ -133,3 +133,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['http://mutualibri-a08-tk.pbp.cs.ui.ac.id/']
