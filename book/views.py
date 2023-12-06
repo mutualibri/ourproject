ff7d645275bd8a8e1e6cbe34c04674054d132c8b
sabrinaatha <sabrinaathashania@gmail.com>
2023-12-06 14:10:30 +0700

    Revert "change workflow"
    This reverts commit afc8dd8857a79175e68e6f21f69d767a300094c9.

--- book/views.py
+++ book/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 
+
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
