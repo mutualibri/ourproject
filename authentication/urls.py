b6ba0f3faed968b0e0dfb9a6689f6d5037bac306
Samuelwidjaja <james.samuel.widjaja@gmail.com>
2023-12-16 03:03:51 +0700

    REGISTER DONE

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from authentication.views import login, logout
+from authentication.views import login, logout, register_flutter
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
+    path('register/', register_flutter, name='register'),
 ]
